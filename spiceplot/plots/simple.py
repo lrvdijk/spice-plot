88253860ea05d40d64b7808dde8eb3da1bb6d563
Lucas van Dijk <info@return1.net>
2014-10-12 12:25:41 +0200

    Fixed a few bugs with accessing the wrong properties

--- spiceplot/plots/simple.py
+++ spiceplot/plots/simple.py
@@ -53,8 +53,9 @@ class LinePlot:
     def generate_axis(self, ax, plot_data):
         for vector in plot_data.vectors:
             plot_func = getattr(ax, self.plot_func, 'plot')
-            plot_func(plot_data.x_axis, vector.data, **self.plot_args)
+            plot_func(plot_data.x_axis.data, vector.data, **self.plot_args)
 
+        ax.grid(which='both')
         ax.legend([vector.name for vector in plot_data.vectors])
         ax.set_title(plot_data.title)
 
@@ -63,7 +64,7 @@ class MultipleLinePlot(LinePlot):
         Create a figure with multiple line plots for each given plot data object
     """
 
-    def __init__(self, plot_datas, plot_func=plt.plot, **kwargs):
+    def __init__(self, plot_datas, plot_func='plot', **kwargs):
         LinePlot.__init__(self, plot_datas, plot_func, **kwargs)
 
         self._x_label = [""]
@@ -96,10 +97,10 @@ class MultipleLinePlot(LinePlot):
 
         for i, plot in enumerate(self.plot_data):
             ax = figure.add_subplot(len(self.plot_data), 1, i+1)
-            ax.set_xlabel(self.x_label[i]
-                if i < len(self.x_label) else self.x_label[0])
-            ax.set_ylabel(self.y_label[i]
-                if i < len(self.y_label) else self.y_label[0])
+            ax.set_xlabel(self._x_label[i]
+                if i < len(self._x_label) else self._x_label[0])
+            ax.set_ylabel(self._y_label[i]
+                if i < len(self._y_label) else self._y_label[0])
 
             self.generate_axis(ax, plot)
 
